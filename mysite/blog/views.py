10e7b4e867d4aa94b827fa194b7ebb41904dfaa4
Karol-05 <2005akaroline@gmail.com>
2026-09-15 21:29:07 -0300

    Adiciona template para exibir posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def index(request):
+    post = Post.objects.first()
+
+    return render(request, "index.html", {"post": post})
