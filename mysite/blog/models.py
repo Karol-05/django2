837f3ecfbc4f422e43bb3a8d0cc90f4e3b35ce57
Karol-05 <2005akaroline@gmail.com>
2026-09-17 20:14:49 -0300

    Ajusta modelo Post para o Admin

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 
 class Post(models.Model):
+    STATUS = (
+        (0, "Rascunho"),
+        (1, "Publicado"),
+    )
+
     title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    status = models.IntegerField(default=0)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
